28fe2aa20d16650fde96ffadfe9fea98c32e2f58
Charlie Phan <charliegphan@gmail.com>
2018-09-07 15:42:14 -0700

    complete seed script for amenities

--- database/seedScript.js
+++ database/seedScript.js
@@ -1,5 +1,11 @@
 const { addDescription, addAmenity } = require('./model.js');
 
+const getRandomInt = (min, max) => {
+  const newMin = Math.ceil(min);
+  const newMax = Math.floor(max);
+  return Math.floor(Math.random() * (newMax - newMin)) + min;
+};
+
 const basics = [
   { name: 'Wifi', details: 'continuous access in the listing' },
   { name: 'Cable TV', details: '' },
@@ -14,6 +20,7 @@ const basics = [
   { name: 'Heating', details: '' },
   { name: 'Stove', details: '' },
 ];
+const basicsRange = getRandomInt(3, basics.length);
 
 const dining = [
   { name: 'Kitchen', details: 'Space where guests can cook their own meals' },
@@ -21,7 +28,9 @@ const dining = [
   { name: 'Cooking Basics', deatails: 'Pots and pans, oil, salt and pepper' },
   { name: 'Microwave', details: '' },
   { name: 'Refrigerator', details: '' },
-  { name: 'Dishes and silverware', details: '' }];
+  { name: 'Dishes and silverware', details: '' }
+];
+const diningRange = getRandomInt(3, dining.length);
 
 const guestAccess = [
   { name: 'Host greets you', details: '' },
@@ -29,8 +38,8 @@ const guestAccess = [
   { name: 'Private entrance', details: 'Separate street or building entrance' },
   { name: 'Building staff', details: 'Someone is available 24 hours a day to let guests in' },
   { name: 'Lockbox', details: '' },
-  {},
-  {}];
+];
+const guestAccessRange = getRandomInt(3, guestAccess.length);
 
 const bedAndBath = [
   { name: 'Hangers', details: '' },
@@ -43,18 +52,80 @@ const bedAndBath = [
     name: 'Lock on bedroom door',
     details: 'Private room can be locked for safety and privacy',
   }];
-
+const bedAndBathRange = getRandomInt(3, bedAndBath.length);
 
 const safetyFeatures = [
   { name: 'Fire extinguisher', details: '' },
   { name: 'Smoke detector', details: '' },
   { name: 'Carbon Monoxide Detector', details: '' },
-  { name: '', details: '' },
+  { name: 'Security guard', details: 'They\'re a cool person' },
   { name: '', details: '' }];
+const safetyFeaturesRange = getRandomInt(3, safetyFeatures.length);
 
 const notIncluded = ['Private entrance', 'Washer', 'Dryer', 'TV', 'Bathroom', 'Bedroom', 'Dogs'];
+const notIncludedRange = getRandomInt(3, notIncluded.length);
 
+const seedDatabaseWithAmenities = () => {
+  const listings = [];
 
-// const seedDatabaseWithAmenities = () => {
+  for (let i = 1; i < 101; i += 1) {
+    const amenity = {};
+    amenity.id = i;
+    amenity.basics = [];
+    amenity.dining = [];
+    amenity.guestAccess = [];
+    amenity.bedAndBath = [];
+    amenity.safetyFeatures = [];
+    amenity.notIncluded = [];
 
-// };
+    for (let j = 0; j < basicsRange; j += 1) {
+      const randomBasics = getRandomInt(0, basics.length);
+      if (!amenity.basics.includes(basics[randomBasics])) {
+        amenity.basics.push(basics[randomBasics]);
+      }
+    }
+
+    for (let j = 0; j < diningRange; j += 1) {
+      const randomDining = getRandomInt(0, dining.length);
+      if (!amenity.dining.includes(dining[randomDining])) {
+        amenity.dining.push(dining[randomDining]);
+      }
+    }
+
+    for (let j = 0; j < guestAccessRange; j += 1) {
+      const randomGuestAccess = getRandomInt(0, guestAccess.length);
+      if (!amenity.guestAccess.includes(guestAccess[randomGuestAccess])) {
+        amenity.guestAccess.push(guestAccess[randomGuestAccess]);
+      }
+    }
+
+    for (let j = 0; j < bedAndBathRange; j += 1) {
+      const randomBedBath = getRandomInt(0, bedAndBath.length);
+      if (!amenity.bedAndBath.includes(bedAndBath[randomBedBath])) {
+        amenity.bedAndBath.push(bedAndBath[randomBedBath]);
+      }
+    }
+
+    for (let j = 0; j < safetyFeaturesRange; j += 1) {
+      const randomSafeyFeatures = getRandomInt(0, safetyFeatures.length);
+      if (!amenity.safetyFeatures.includes(safetyFeatures[randomSafeyFeatures])) {
+        amenity.safetyFeatures.push(safetyFeatures[randomSafeyFeatures]);
+      }
+    }
+
+    for (let j = 0; j < notIncludedRange; j += 1) {
+      const randomNotIncluded = getRandomInt(0, notIncluded.length);
+      if (!amenity.notIncluded.includes(notIncluded[randomNotIncluded])) {
+        amenity.notIncluded.push(notIncluded[randomNotIncluded]);
+      }
+    }
+
+    listings.push(amenity);
+  }
+
+  listings.forEach((listing) => {
+    addAmenity(listing);
+  });
+};
+
+seedDatabaseWithAmenities();
