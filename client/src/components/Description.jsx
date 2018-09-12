5f390aec13d692d1a88141905a3ecf45ae86c6ba
Charlie Phan <charliegphan@gmail.com>
2018-09-11 20:58:22 -0700

    add Description CSS

--- client/src/components/Description.jsx
+++ client/src/components/Description.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import styles from '../../../styles/Description.css';
 
 const Description = ({ description }) => (
-  <div>
-    {description}
+  <div className={styles.container}>
+    <div>
+      {description}
+    </div>
+    <button
+      className={styles.button}
+      type="button"
+    >Read more about the space
+    </button>
+
   </div>
 );
 
