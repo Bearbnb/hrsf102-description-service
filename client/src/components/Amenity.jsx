950401cf57f878c62243cf0eb6061a04ccbb834e
Charlie Phan <charliegphan@gmail.com>
2018-09-17 14:38:23 -0700

    map amenities keys to formated titled

--- client/src/components/Amenity.jsx
+++ client/src/components/Amenity.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Amenity = ({ availableAmenity }) => (
-  <div>
-    <h3>{availableAmenity.amenity}</h3>
-    <hr />
-    {availableAmenity.amenities.map((amenity => (
-      <p>{amenity.name}</p>
-    )))}
-  </div>
-);
+const Amenity = ({ availableAmenity }) => {
+  const amenityFormatMap = {
+    basics: 'Basics',
+    guestAccess: 'Guest Access',
+    bedAndBath: 'Bed and Bath',
+    safetyFeatures: 'Safety Features',
+    notIncluded: 'Not Included',
+  };
+
+  return (
+    <div>
+      <h3>{amenityFormatMap[availableAmenity.amenity]}</h3>
+      <hr />
+      {availableAmenity.amenities.map((amenity => (
+        <p>{amenity.name}</p>
+      )))}
+    </div>
+  );
+};
 
 Amenity.propTypes = {
   availableAmenity: PropTypes.shape({
