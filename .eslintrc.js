f5eb2fb9f7427aee6d2d195c76ea27500916ce47
Charlie Phan <charliegphan@gmail.com>
2018-09-18 15:31:35 -0700

    add highlight and thumbs component

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: "airbnb",
   rules: {
     "import/extensions": 0,
-    "no-console": 0,
+    "no-console": 1,
     "no-unused-vars": 1,
     "react/jsx-one-expression-per-line": 0
   },
