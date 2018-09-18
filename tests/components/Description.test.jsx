f735aad595d76944079ed8c52e41c01580579cce
Charlie Phan <charliegphan@gmail.com>
2018-09-18 16:19:35 -0700

    update package json

--- tests/components/Description.test.jsx
+++ tests/components/Description.test.jsx
@@ -1,4 +1,4 @@
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import React from 'react';
 import toJson from 'enzyme-to-json';
 import Description from '../../client/src/components/Description';
