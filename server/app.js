4d12bfbbe37f527843e1975af40286679ecd20e9
Charlie Phan <charliegphan@gmail.com>
2018-09-19 11:50:00 -0700

    update project to be proxy friendly

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 const { getListing } = require('../database/model.js');
 
+app.use(cors());
 app.use('/:id', express.static(path.resolve(__dirname, '../public')));
+app.use('/static', express.static('public'));
 
 app.get('/listings/:id', (req, res) => {
   getListing(req.params.id, (listing) => {
