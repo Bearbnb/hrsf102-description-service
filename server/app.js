78db5a2628b9f56e508395a74c5ed7330afc8480
Charlie Phan <charliegphan@gmail.com>
2018-09-18 17:26:16 -0700

    add app.js file

---
+++ server/app.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+const { getListing } = require('../database/model.js');
+
+app.use('/:id', express.static(path.resolve(__dirname, '../public')));
+
+
+app.get('/listings/:id', (req, res) => {
+  getListing(req.params.id, (listing) => {
+    res.send(listing);
+  });
+});
+
+module.exports = app;
